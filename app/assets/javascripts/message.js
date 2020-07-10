6686066d6f147d6b15b2f6d5ae4d73b034fdb57b
f4rk19 <67687475+f4rk19@users.noreply.github.com>
2020-07-10 20:24:45 +0900

    Ajax

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,65 @@
+$(function(){
+  function buildHTML(message){
+    if (message.image){
+      let html = 
+      `<div class="message-box">
+        <div class="message-info">
+          <div class="message-info__user-name">
+            ${message.user_name}
+          </div>
+          <div class="message-info__created_at">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message">
+          <p class="message__content">
+            ${message.content}
+          </p>
+          <img class="message__image" src="${message.image}">
+        </div>
+      </div>`
+      return html;
+  } else{
+    let html =
+      `<div class="message-box">
+        <div class="message-info">
+          <div class="messageinfo__user-name">
+            ${message.user_name}
+          </div>
+          <div class="message-info__created_at">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message">
+          <p class="message__content">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+      return html;
+  };
+}
+  $('.input-box').on('submit', function(e){
+    e.preventDefault();
+    let formData = new FormData(this);
+    let url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      let html =buildHTML(data);
+      $('.main_chat__message-list').append(html);
+      $('.main_chat__message-list').animate({ scrollTop: $('.main_chat__message-list')[0].scrollHeight});
+      $('form')[0].reset();
+      $('.submit-btn').prop('disabled', false);
+    })
+    .fail(function(){
+      alert('メッセージ送信に失敗しました')
+    })
+  })
+});
